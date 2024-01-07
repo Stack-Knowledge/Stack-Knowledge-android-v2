affccdf981449952645d9f42c8412afb4172a0fb
yeongun130 <yeongunj625@gmail.com>
2024-01-07 20:19:31 +0900

    :lipstick: set up design system

--- core/design-system/src/main/java/com/stackknowledge/design_system/theme/color/ColorTheme.kt
+++ core/design-system/src/main/java/com/stackknowledge/design_system/theme/color/ColorTheme.kt
@@ -4,9 +4,15 @@ import androidx.compose.ui.graphics.Color
 
 abstract class ColorTheme {
 
+    //Primary
+    abstract val P1: Color
+
     // Greyscale
     abstract val BLACK: Color
     abstract val G1: Color
     abstract val G2: Color
+    abstract val G3: Color
+    abstract val G4: Color
+    abstract val G5: Color
     abstract val WHITE: Color
 }
