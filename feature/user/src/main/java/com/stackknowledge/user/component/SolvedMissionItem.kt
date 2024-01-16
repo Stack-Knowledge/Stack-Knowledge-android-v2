e6b5a6349f30ff73b57fb412b85a17c0cc82b349
Chaejongin12 <s22055@gsm.hs.kr>
2024-01-17 01:51:27 +0900

    :lipstick: :: SolvedMissionItem Box로 Scope 변경
    :: SolvedMissionItem Box로 Scope 변경

--- feature/user/src/main/java/com/stackknowledge/user/component/SolvedMissionItem.kt
+++ feature/user/src/main/java/com/stackknowledge/user/component/SolvedMissionItem.kt
@@ -2,6 +2,7 @@ package com.stackknowledge.user.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,14 +12,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.stackknowledge.design_system.R
 import com.stackknowledge.design_system.theme.StackKnowledgeAndroidTheme
@@ -28,15 +40,14 @@ fun SolvedMissionItem(
     modifier: Modifier = Modifier,
 ) {
     StackKnowledgeAndroidTheme { colors, typography ->
-        Surface(
+
+        Box(
             modifier = modifier
                 .clip(shape = RoundedCornerShape(20.dp))
-                .background(
-                    color = colors.WHITE,
-                )
+                .background(color = colors.WHITE)
                 .width(156.dp)
                 .height(180.dp),
-            shadowElevation = 8.dp
+            contentAlignment = Alignment.Center
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -74,7 +85,6 @@ fun SolvedMissionItem(
                     )
                 }
             }
-
         }
     }
 }
