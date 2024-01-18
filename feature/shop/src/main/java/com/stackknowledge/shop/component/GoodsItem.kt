098fdf536535051f1ff7ccc1c5f0cc71589ffd11
yeongun130 <yeongunj625@gmail.com>
2024-01-18 16:39:14 +0900

    :recycle: GoodsItem Check Component 수정

--- feature/shop/src/main/java/com/stackknowledge/shop/component/GoodsItem.kt
+++ feature/shop/src/main/java/com/stackknowledge/shop/component/GoodsItem.kt
@@ -2,6 +2,7 @@ package com.stackknowledge.shop.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,8 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,9 +28,15 @@ import com.stackknowledge.design_system.theme.StackKnowledgeAndroidTheme
 
 @Composable
 fun GoodsItem(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    var checked by remember { mutableStateOf(false) }
+    var checked by remember { mutableStateOf(true) }
+    
+    val imageResource = if(checked) {
+        painterResource(R.drawable.uncheck_image)
+    } else {
+        painterResource(R.drawable.check_image)
+    }
 
     StackKnowledgeAndroidTheme { colors, typography ->
         Column(
@@ -42,20 +48,23 @@ fun GoodsItem(
                     painter = painterResource(R.drawable.goods_image),
                     contentDescription = "Goods Image"
                 )
-
-                Checkbox(
-                    checked = checked,
-                    onCheckedChange = { checked = it },
-                    colors = CheckboxDefaults.colors(
-                        checkedColor = colors.G7,
-                        uncheckedColor = colors.WHITE,
-                        checkmarkColor = colors.P1
-                    ),
+                Box(
                     modifier = modifier
-                        .width(16.dp)
-                        .height(16.dp)
-                        .padding(start = 16.dp, top = 16.dp)
-                )
+                        .padding(start = 8.dp, top = 8.dp)
+                ) {
+                    Image(
+                        painter = imageResource,
+                        contentDescription = null,
+                        modifier = modifier
+                            .width(16.dp)
+                            .height(16.dp)
+                            .clickable {
+                                checked = !checked
+                            }
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
 
             Spacer(modifier = modifier.height(4.dp))
