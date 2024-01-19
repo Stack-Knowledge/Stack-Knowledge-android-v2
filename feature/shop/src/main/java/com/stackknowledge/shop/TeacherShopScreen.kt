ccb11ecec23ff27f2015e594c3940c6ac52111e4
Chaejongin12 <s22055@gsm.hs.kr>
2024-01-19 16:43:22 +0900

    :lipstick: :: TeacherShopScreen Component 생성
    :: TeacherShopScreen Component 생성

---
+++ feature/shop/src/main/java/com/stackknowledge/shop/TeacherShopScreen.kt
@@ -0,0 +1,37 @@
+package com.stackknowledge.shop
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.stackknowledge.design_system.component.topbar.StackKnowledgeTopBar
+import com.stackknowledge.design_system.theme.StackKnowledgeAndroidTheme
+
+@Composable
+fun TeacherShoScreen(
+    modifier: Modifier = Modifier
+) {
+    StackKnowledgeAndroidTheme { colors, typography ->
+        Surface (
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = modifier
+                    .background(color = colors.WHITE)
+            ) {
+                StackKnowledgeTopBar()
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TeacherShoScreenPre() {
+    TeacherShoScreen()
+}
