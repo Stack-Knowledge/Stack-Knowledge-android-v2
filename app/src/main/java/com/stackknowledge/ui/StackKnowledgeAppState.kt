e7f33256d54c60e45c7a6af4b4ab2a28b78e1485
Chaejongin12 <s22055@gsm.hs.kr>
2024-01-24 01:07:18 +0900

    :sparkles: :: StackKnowledgeAppState작성
    :: StackKnowledgeAppState작성

--- app/src/main/java/com/stackknowledge/ui/StackKnowledgeAppState.kt
+++ app/src/main/java/com/stackknowledge/ui/StackKnowledgeAppState.kt
@@ -1,5 +1,55 @@
 package com.stackknowledge.ui
 
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.stackknowledge.navigation.TopLevelDestination
 import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun rememberStackKnowledgeAppState(
+    windowSizeClass: WindowSizeClass,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavController = rememberNavController(),
+): StackKnowledgeAppState {
+    return remember(
+        navController,
+        coroutineScope,
+        windowSizeClass
+    ) {
+        StackKnowledgeAppState(
+            navController = navController,
+            coroutineScope = coroutineScope,
+            windowSizeClass = windowSizeClass
+        )
+    }
+}
+
+@Stable
+class StackKnowledgeAppState(
+    val navController: NavController,
+    val coroutineScope: CoroutineScope,
+    val windowSizeClass: WindowSizeClass
+) {
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when(currentDestination?.route) {
+            // loginRoute 작성
+            else -> null
+        }
+
+    val shouldShowBottomBar: Boolean
+        get() = windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact
+
+    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
+}
