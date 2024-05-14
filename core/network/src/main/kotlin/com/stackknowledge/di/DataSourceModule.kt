cbf8ec137db1b70462668f1c5132d66a636cfb9a
yeongun130 <yeongunj625@gmail.com>
2024-05-14 09:59:30 +0900

    :memo: :: add module

---
+++ core/network/src/main/kotlin/com/stackknowledge/di/DataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.stackknowledge.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+}
