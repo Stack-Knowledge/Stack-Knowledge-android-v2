840c927f37742ba7f50da5eff7cb9d493e187564
Chaejongin12 <s22055@gsm.hs.kr>
2024-05-15 15:21:43 +0900

    :sparkles: :: StudentDataSource suspend fun return 작성
    :: StudentDataSource suspend fun return 작성

--- core/network/src/main/kotlin/com/stackknowledge/datasource/student/StudentDataSourceImpl.kt
+++ core/network/src/main/kotlin/com/stackknowledge/datasource/student/StudentDataSourceImpl.kt
@@ -1,7 +1,11 @@
 package com.stackknowledge.datasource.student
 
 import com.stackknowledge.api.StudentAPI
+import com.stackknowledge.util.StackKnowledgeApiHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import remote.request.student.UploadProfileImageRequest
 import remote.response.student.GetMyInformationResponse
 import remote.response.student.GetStudentPointRankingResponse
@@ -9,17 +13,30 @@ import remote.response.student.UploadProfileImageResponse
 import javax.inject.Inject
 
 class StudentDataSourceImpl @Inject constructor(
-    private val studentAPI: StudentAPI
+    private val studentAPI: StudentAPI,
 ) : StudentDataSource {
-    override suspend fun getStudentPointRanking(): Flow<List<GetStudentPointRankingResponse>> {
-        TODO()
-    }
+    override suspend fun getStudentPointRanking(): Flow<List<GetStudentPointRankingResponse>> =
+        flow {
+            emit(
+                StackKnowledgeApiHandler<List<GetStudentPointRankingResponse>>()
+                    .httpRequest { studentAPI.getStudentPointRanking() }
+                    .sendRequest()
+            )
+        }.flowOn(Dispatchers.IO)
 
-    override suspend fun getMyInformation(): Flow<GetMyInformationResponse> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getMyInformation(): Flow<GetMyInformationResponse> = flow {
+        emit(
+            StackKnowledgeApiHandler<GetMyInformationResponse>()
+                .httpRequest { studentAPI.getMyInformation() }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
 
-    override suspend fun uploadProfileImage(body: UploadProfileImageRequest): Flow<UploadProfileImageResponse> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun uploadProfileImage(body: UploadProfileImageRequest): Flow<UploadProfileImageResponse> = flow {
+        emit(
+            StackKnowledgeApiHandler<UploadProfileImageResponse>()
+                .httpRequest { studentAPI.uploadProfileImage(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
 }
