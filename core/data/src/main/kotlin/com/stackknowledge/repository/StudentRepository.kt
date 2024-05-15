45cd34e62b3ac06ee847b3687c77003d07d4f041
Chaejongin12 <s22055@gsm.hs.kr>
2024-05-15 23:48:19 +0900

    :fire: :: StudentRepository 공백 수정
    :: StudentRepository 공백 수정

--- core/data/src/main/kotlin/com/stackknowledge/repository/StudentRepository.kt
+++ core/data/src/main/kotlin/com/stackknowledge/repository/StudentRepository.kt
@@ -7,7 +7,7 @@ import remote.response.student.GetStudentPointRankingResponse
 import remote.response.student.UploadProfileImageResponse
 
 interface StudentRepository {
-    suspend fun getStudentPointRaking() : Flow<List<GetStudentPointRankingResponse>>
+    suspend fun getStudentPointRaking(): Flow<List<GetStudentPointRankingResponse>>
     suspend fun getMyInformation(): Flow<GetMyInformationResponse>
     suspend fun uploadProfileImage(body: UploadProfileImageRequest): Flow<UploadProfileImageResponse>
 }
