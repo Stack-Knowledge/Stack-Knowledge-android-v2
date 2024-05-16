7ff56b3d6bf0c3734a295a622c0dac827c79de26
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 14:39:47 +0900

    :memo: :: Add response mapper

---
+++ core/network/src/main/kotlin/com/stackknowledge/mapper/response/GetRequestSignUpTeacherResponseMapper.kt
@@ -0,0 +1,11 @@
+package com.stackknowledge.mapper.response
+
+import com.stackknowledge.dto.response.user.GetRequestSignUpTeacherResponse
+import remote.response.user.GetRequestSignUpTeacherResponseModel
+
+fun GetRequestSignUpTeacherResponse.toModel(): GetRequestSignUpTeacherResponseModel =
+    GetRequestSignUpTeacherResponseModel(
+        userId = this.userId,
+        name = this.name,
+        createdAt = this.createdAt
+    )
---
+++ core/network/src/main/kotlin/com/stackknowledge/mapper/user/UserMapper.kt
@@ -0,0 +1,11 @@
+package com.stackknowledge.mapper.user
+
+import com.stackknowledge.dto.user.User
+import remote.user.UserModel
+
+fun User.toModel(): UserModel =
+    UserModel(
+        id = this.id,
+        name = this.name,
+        profileImage = this.profileImage
+    )
