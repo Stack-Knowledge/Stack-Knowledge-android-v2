29ae14553b3bd73b2d148ed5374529032fb824d0
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 11:46:02 +0900

    :heavy_plus_sign: :: Add moshi dependencies

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -30,7 +30,10 @@ dependencies {
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
-    implementation(libs.retrofit.gson.converter)
+    //implementation(libs.retrofit.gson.converter) Gson 사용 여부 협의후 삭제
+    implementation(libs.retrofit.moshi.converter)
+    implementation(libs.moshi)
+    ksp(libs.retrofit.moshi.codegen)
 }
 fun getApiKey(propertyKey: String): String {
     val propFile = rootProject.file("./local.properties")
