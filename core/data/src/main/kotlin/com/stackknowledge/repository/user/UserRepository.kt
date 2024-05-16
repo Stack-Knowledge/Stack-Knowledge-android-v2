fe9b5669bfd66f2e1a8d7fa28ee885c2185d1188
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 14:45:31 +0900

    :memo: :: Modify UserRepository

--- core/data/src/main/kotlin/com/stackknowledge/repository/user/UserRepository.kt
+++ core/data/src/main/kotlin/com/stackknowledge/repository/user/UserRepository.kt
@@ -6,16 +6,21 @@ import com.stackknowledge.dto.response.user.DetailSolveMissionResponse
 import com.stackknowledge.dto.response.user.GetRequestSignUpTeacherResponse
 import com.stackknowledge.dto.response.user.GetSolveMissionResponse
 import kotlinx.coroutines.flow.Flow
+import remote.request.user.ApproveRequestModel
+import remote.request.user.ScoreRequestModel
+import remote.response.user.DetailSolveMissionResponseModel
+import remote.response.user.GetRequestSignUpTeacherResponseModel
+import remote.response.user.GetSolveMissionResponseModel
 import java.util.UUID
 
 interface UserRepository {
-    fun getSolvedMission(): Flow<List<GetSolveMissionResponse>>
+    fun getSolvedMission(): Flow<List<GetSolveMissionResponseModel>>
 
-    fun getDetailSolveMission(solveId: UUID): Flow<DetailSolveMissionResponse>
+    fun getDetailSolveMission(solveId: UUID): Flow<DetailSolveMissionResponseModel>
 
-    fun getRequestSignUpTeacher(): Flow<List<GetRequestSignUpTeacherResponse>>
+    fun getRequestSignUpTeacher(): Flow<List<GetRequestSignUpTeacherResponseModel>>
 
-    fun scoreSolveMission(solveId: UUID, body: ScoreRequest): Flow<Unit>
+    fun scoreSolveMission(solveId: UUID, body: ScoreRequestModel): Flow<Unit>
 
-    fun signUpApprove(userId: UUID, body: ApproveRequest): Flow<Unit>
+    fun signUpApprove(userId: UUID, body: ApproveRequestModel): Flow<Unit>
 }
--- core/data/src/main/kotlin/com/stackknowledge/repository/user/UserRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/stackknowledge/repository/user/UserRepositoryImpl.kt
@@ -6,36 +6,44 @@ import com.stackknowledge.dto.request.user.ScoreRequest
 import com.stackknowledge.dto.response.user.DetailSolveMissionResponse
 import com.stackknowledge.dto.response.user.GetRequestSignUpTeacherResponse
 import com.stackknowledge.dto.response.user.GetSolveMissionResponse
+import com.stackknowledge.mapper.request.user.toDto
+import com.stackknowledge.mapper.response.toModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import remote.request.user.ApproveRequestModel
+import remote.request.user.ScoreRequestModel
+import remote.response.user.DetailSolveMissionResponseModel
+import remote.response.user.GetRequestSignUpTeacherResponseModel
+import remote.response.user.GetSolveMissionResponseModel
 import java.util.UUID
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val userDataSource: UserDataSource
 ) : UserRepository {
-    override fun getSolvedMission(): Flow<List<GetSolveMissionResponse>> {
-        return userDataSource.getSolvedMission()
+    override fun getSolvedMission(): Flow<List<GetSolveMissionResponseModel>> {
+        return userDataSource.getSolvedMission().map { list -> list.map { it.toModel() } }
     }
 
-    override fun getDetailSolveMission(solveId: UUID): Flow<DetailSolveMissionResponse> {
-        return userDataSource.getDetailSolveMission(solveId = solveId)
+    override fun getDetailSolveMission(solveId: UUID): Flow<DetailSolveMissionResponseModel> {
+        return userDataSource.getDetailSolveMission(solveId = solveId).map { it.toModel() }
     }
 
-    override fun getRequestSignUpTeacher(): Flow<List<GetRequestSignUpTeacherResponse>> {
-        return userDataSource.getRequestSignUpTeacher()
+    override fun getRequestSignUpTeacher(): Flow<List<GetRequestSignUpTeacherResponseModel>> {
+        return userDataSource.getRequestSignUpTeacher().map { list -> list.map { it.toModel() } }
     }
 
-    override fun scoreSolveMission(solveId: UUID, body: ScoreRequest): Flow<Unit> {
+    override fun scoreSolveMission(solveId: UUID, body: ScoreRequestModel): Flow<Unit> {
         return userDataSource.scoreSolveMission(
             solveId = solveId,
-            body = body
+            body = body.toDto()
         )
     }
 
-    override fun signUpApprove(userId: UUID, body: ApproveRequest): Flow<Unit> {
+    override fun signUpApprove(userId: UUID, body: ApproveRequestModel): Flow<Unit> {
         return userDataSource.signUpApprove(
             userId = userId,
-            body = body
+            body = body.toDto()
         )
     }
 }
