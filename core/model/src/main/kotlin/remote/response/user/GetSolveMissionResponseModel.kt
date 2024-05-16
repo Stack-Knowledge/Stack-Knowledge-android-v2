ba9d58408a0931e01f136470c3967b220683ab6e
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 14:16:58 +0900

    :memo: :: Add response model

---
+++ core/model/src/main/kotlin/remote/response/user/GetSolveMissionResponseModel.kt
@@ -0,0 +1,12 @@
+package remote.response.user
+
+import remote.user.UserModel
+import java.util.UUID
+
+data class GetSolveMissionResponseModel(
+    val solveId: UUID,
+    val solveStatus: String,
+    val title: String,
+    val point: Int,
+    val user: UserModel
+)
---
+++ core/model/src/main/kotlin/remote/user/UserModel.kt
@@ -0,0 +1,9 @@
+package remote.user
+
+import java.util.UUID
+
+data class UserModel (
+    val id: UUID,
+    val name: String,
+    val profileImage: String?
+)
