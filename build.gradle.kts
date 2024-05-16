415a31f7a724abf9d4fa18786a457fdddab695d0
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 11:45:35 +0900

    :hammer: :: Migrate kapt -> ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION")
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
-
 }
 
 plugins {
@@ -14,4 +13,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
 }
