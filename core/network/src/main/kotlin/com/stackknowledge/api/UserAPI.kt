d37c047ee90a81fcb6356c5263d13562a9d2cb81
GSM-Minseok <ms0818000@gmail.com>
2024-05-16 12:20:01 +0900

    :memo: :: Add api user/{user_id}

--- core/network/src/main/kotlin/com/stackknowledge/api/UserAPI.kt
+++ core/network/src/main/kotlin/com/stackknowledge/api/UserAPI.kt
@@ -1,11 +1,13 @@
 package com.stackknowledge.api
 
+import com.stackknowledge.dto.request.user.ApproveRequest
 import com.stackknowledge.dto.request.user.ScoreRequest
 import com.stackknowledge.dto.response.user.DetailSolveMissionResponse
 import com.stackknowledge.dto.response.user.GetRequestSignUpTeacherResponse
 import com.stackknowledge.dto.response.user.GetSolveMissionResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 import java.util.UUID
@@ -26,5 +28,11 @@ interface UserAPI {
     suspend fun scoreSolveMission(
         @Path("solve_id") solveId: UUID,
         @Body body: ScoreRequest,
+    )
+
+    @PATCH("/user/{user_id}")
+    suspend fun signUpApprove(
+        @Path("user_id") userId: UUID,
+        @Body body: ApproveRequest
     )
 }
