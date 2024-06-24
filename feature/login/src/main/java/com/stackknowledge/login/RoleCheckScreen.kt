04c64effbd022393942634f8fc4661a01f49560f
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-24 13:58:52 +0900

    :lipstick: :: RoleCheckScreen 퍼블리싱
    :: RoleCheckScreen 퍼블리싱

---
+++ feature/login/src/main/java/com/stackknowledge/login/RoleCheckScreen.kt
@@ -0,0 +1,81 @@
+package com.stackknowledge.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.stackknowledge.design_system.component.button.StackKnowledgeButton
+import com.stackknowledge.design_system.theme.StackKnowledgeAndroidTheme
+import com.stackknowledge.login.background.LoginBackground
+
+@Composable
+fun RoleCheckScreen(
+    modifier: Modifier = Modifier,
+) {
+    StackKnowledgeAndroidTheme { colors, typography ->
+        Box(
+            modifier = modifier.background(color = colors.WHITE),
+        ) {
+            LoginBackground()
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = modifier.height(260.dp))
+
+                Text(
+                    text = "사용자가 학생인지 \n" + "선생님인지 선택해주세요.",
+                    style = typography.titleMedium,
+                    color = colors.BLACK,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = modifier.weight(1f))
+
+                Row(
+                    modifier = modifier.fillMaxWidth()
+                ) {
+                    StackKnowledgeButton(
+                        text = "학생",
+                        modifier = modifier
+                            .height(60.dp)
+                            .weight(1f)
+                    )
+
+                    Spacer(modifier = modifier.width(8.dp))
+
+                    StackKnowledgeButton(
+                        text = "선생님",
+                        modifier = modifier
+                            .height(60.dp)
+                            .weight(1f)
+                    )
+                }
+
+                Spacer(modifier = modifier.height(102.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RoleCheckScreenPre() {
+    RoleCheckScreen()
+}
