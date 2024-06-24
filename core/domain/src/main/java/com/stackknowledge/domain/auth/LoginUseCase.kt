818ebc1bbea1ae649d0873b723a43dde4ba93391
yeongun130 <yeongunj625@gmail.com>
2024-06-24 17:06:49 +0900

    :memo: :: add common module

--- core/domain/src/main/java/com/stackknowledge/domain/auth/LoginUseCase.kt
+++ core/domain/src/main/java/com/stackknowledge/domain/auth/LoginUseCase.kt
@@ -10,7 +10,7 @@ class LoginUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(
         body: LoginRequest,
-        role: Authority
+        role: String
     ) = runCatching {
         authRepository.login(
             body = body,
