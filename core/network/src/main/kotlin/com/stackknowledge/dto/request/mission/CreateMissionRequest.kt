58804bd3723128475949590b9f75cd95402ea353
yeongun130 <yeongunj625@gmail.com>
2024-06-26 21:43:54 +0900

    Merge branch 'develop' of https://github.com/Stack-Knowledge/Stack-Knowledge-android-v2 into feature/#46_mission_domain_network_setting
    # Conflicts:
    #	core/data/src/main/kotlin/com/stackknowledge/di/RepositoryModule.kt
    #	core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
    #	core/network/src/main/kotlin/com/stackknowledge/di/RemoteDataSourceModule.kt

--- core/network/src/main/kotlin/com/stackknowledge/dto/request/mission/CreateMissionRequest.kt
+++ core/network/src/main/kotlin/com/stackknowledge/dto/request/mission/CreateMissionRequest.kt
@@ -1,6 +1,5 @@
 package com.stackknowledge.dto.request.mission
 
-<<<<<<< HEAD
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -10,10 +9,3 @@ data class CreateMissionRequest (
     @Json(name = "content") val content: String,
     @Json(name = "timeLimit") val timeLimit: Int,
 )
-=======
-data class CreateMissionRequest(
-    val title: String,
-    val content: String,
-    val timeLimit: Int,
-)
->>>>>>> origin/feature/#46_mission_domain_network_setting
