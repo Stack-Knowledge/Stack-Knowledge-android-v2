7c47875452fb42219ef6ad37f3ae7224d0a8c0b8
yeongun130 <yeongunj625@gmail.com>
2024-06-26 22:21:51 +0900

    :memo: :: @Singleton 추가

--- core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
@@ -7,15 +7,18 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
+    @Singleton
     fun provideMissionAPI(retrofit: Retrofit): MissionAPI =
         retrofit.create(MissionAPI::class.java)
 
     @Provides
+    @Singleton
     fun provideStudentAPI(retrofit: Retrofit): StudentAPI =
         retrofit.create(StudentAPI::class.java)
 }
--- core/network/src/main/kotlin/com/stackknowledge/di/RemoteDataSourceModule.kt
+++ core/network/src/main/kotlin/com/stackknowledge/di/RemoteDataSourceModule.kt
@@ -8,16 +8,19 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RemoteDataSourceModule {
     @Binds
+    @Singleton
     abstract fun bindMissionDataSource(
         missionDataSourceImpl: MissionDataSourceImpl
     ): MissionDataSource
 
     @Binds
+    @Singleton
     abstract fun bindStudentDataSource(
         studentDataSourceImpl: StudentDataSourceImpl
     ): StudentDataSource
