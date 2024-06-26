36c80a9b5ac41f4b595ca4441b3597e2c7cf8ffb
yeongun130 <yeongunj625@gmail.com>
2024-06-26 20:13:23 +0900

    :sparkles: :: Mission NetworkModule에 추가

--- core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
@@ -1,9 +1,19 @@
 package com.stackknowledge.di
 
+import com.stackknowledge.api.MissionAPI
+import com.stackknowledge.datasource.mission.MissionDataSource
+import com.stackknowledge.datasource.mission.MissionDataSourceImpl
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {}
+object NetworkModule {
+    @Provides
+    fun provideMissionAPI(retrofit: Retrofit): MissionAPI =
+        retrofit.create(MissionAPI::class.java)
+}
--- core/network/src/main/kotlin/com/stackknowledge/di/RemoteDataSourceModule.kt
+++ core/network/src/main/kotlin/com/stackknowledge/di/RemoteDataSourceModule.kt
@@ -1,9 +1,17 @@
 package com.stackknowledge.di
 
+import com.stackknowledge.datasource.mission.MissionDataSource
+import com.stackknowledge.datasource.mission.MissionDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RemoteDataSourceModule {}
+abstract class RemoteDataSourceModule {
+    @Binds
+    abstract fun bindMissionDataSource(
+        missionDataSourceImpl: MissionDataSourceImpl
+    ): MissionDataSource
+}
