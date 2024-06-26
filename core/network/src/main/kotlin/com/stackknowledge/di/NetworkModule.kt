5e333070686dd57f601916b28d867877a0fd554b
yeongun130 <yeongunj625@gmail.com>
2024-06-26 22:39:07 +0900

    :sparkles: :: modify NetWorkModule

--- core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/stackknowledge/di/NetworkModule.kt
@@ -1,17 +1,66 @@
 package com.stackknowledge.di
 
+import com.squareup.moshi.Moshi
 import com.stackknowledge.api.MissionAPI
 import com.stackknowledge.api.StudentAPI
+import com.stackknowledge.network.BuildConfig
+import com.stackknowledge.util.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CookieJar
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cookieJar(CookieJar.NO_COOKIES)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshiInstance(): Moshi {
+        return Moshi.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
     @Provides
     @Singleton
     fun provideMissionAPI(retrofit: Retrofit): MissionAPI =
