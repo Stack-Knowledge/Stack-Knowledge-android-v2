af319dc9e05e9861da0403bef5e08f384c44a1fe
yeongun130 <yeongunj625@gmail.com>
2024-06-27 16:34:54 +0900

    :fire: :: delete import

--- core/model/src/main/kotlin/remote/response/mission/MissionResponseModel.kt
+++ core/model/src/main/kotlin/remote/response/mission/MissionResponseModel.kt
@@ -2,9 +2,6 @@ package remote.response.mission
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import enumdatatype.MissionStatus
-import remote.user.UserModel
-import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class MissionResponseModel(
