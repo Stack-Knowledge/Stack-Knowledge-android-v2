fe79664a1919f5bce63116c2bfe0c667f723bfbb
yeongun130 <yeongunj625@gmail.com>
2024-06-27 12:18:10 +0900

    :sparkles: :: ItemAPI 작성

---
+++ core/network/src/main/kotlin/com/stackknowledge/api/ItemAPI.kt
@@ -0,0 +1,9 @@
+package com.stackknowledge.api
+
+import remote.response.item.GetItemResponseModel
+import retrofit2.http.GET
+
+interface ItemAPI {
+    @GET("/item")
+    suspend fun getItem(): GetItemResponseModel
+}
