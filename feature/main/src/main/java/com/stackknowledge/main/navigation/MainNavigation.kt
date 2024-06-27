0a17aa21376866e9e22949b3e88b25e717fcf90a
GSM-Minseok <ms0818000@gmail.com>
2024-06-27 23:11:07 +0900

    :sparkles: :: Add MainNavigation.kt

---
+++ feature/main/src/main/java/com/stackknowledge/main/navigation/MainNavigation.kt
@@ -0,0 +1,19 @@
+package com.stackknowledge.main.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.stackknowledge.main.MainPageRoute
+
+const val mainPageRoute = "main_page_route"
+
+fun NavController.navigateToMain(navOptions: NavOptions? = null) {
+    this.navigate(mainPageRoute, navOptions)
+}
+
+fun NavGraphBuilder.mainScreen() {
+    composable(route = mainPageRoute) {
+        MainPageRoute()
+    }
+}
