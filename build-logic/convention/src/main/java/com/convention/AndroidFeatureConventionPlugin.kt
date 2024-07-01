00902b21cba4603a48110f20cb623186688d51bb
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-01 16:49:49 +0900

    🔥:: FeatureConvention data, dataStore 의존성 삭

--- build-logic/convention/src/main/java/com/convention/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/convention/AndroidFeatureConventionPlugin.kt
@@ -18,9 +18,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", project(":core:model"))
                 add("implementation", project(":core:ui"))
                 add("implementation", project(":core:design-system"))
-                add("implementation", project(":core:data"))
                 add("implementation", project(":core:domain"))
-                add("implementation", project(":core:datastore"))
                 add("implementation", project(":core:common"))
 
                 add("implementation", libs.findLibrary("coil.kt").get())
