45ce4a8075c6b542065505a8251c4d437a0db01d
yeongun130 <yeongunj625@gmail.com>
2024-07-02 14:56:16 +0900

    :sparkles: :: ViewModel 방식 변경

--- feature/mission/src/main/java/com/stackkowledge/mission/viewmodel/MissionViewModel.kt
+++ feature/mission/src/main/java/com/stackkowledge/mission/viewmodel/MissionViewModel.kt
@@ -1,16 +1,24 @@
 package com.stackkowledge.mission.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.common.result.Result
+import com.example.common.result.asResult
 import com.example.common.util.Event
 import com.example.common.util.errorHandling
 import com.stackknowledge.usecase.misson.CreateMissionUseCase
 import com.stackknowledge.usecase.misson.DetailMissionUseCase
 import com.stackknowledge.usecase.misson.GetMissionUseCase
+import com.stackkowledge.mission.uistate.CreateMissionUiState
+import com.stackkowledge.mission.uistate.GetMissionUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import remote.request.mission.CreateMissionRequestModel
@@ -25,27 +33,40 @@ class MissionViewModel @Inject constructor(
     private val detailMissionUseCase: DetailMissionUseCase,
     private val createMissionUseCase: CreateMissionUseCase,
 ) : ViewModel() {
-    private val _missionRequest = MutableStateFlow<Event<MissionResponseModel>>(Event.Loading)
-    val missionRequest = _missionRequest.asStateFlow()
+    private val _missionUiState = MutableStateFlow<GetMissionUiState>(GetMissionUiState.Loading)
+    val missionUiState = _missionUiState.asStateFlow()
 
     private val _detailMissionRequest =
         MutableStateFlow<Event<DetailMissionResponseModel>>(Event.Loading)
     val detailMissionRequest = _detailMissionRequest.asStateFlow()
 
-    private val _createMissionRequest = MutableStateFlow<Event<Nothing>>(Event.Loading)
-    val createMissionRequest = _createMissionRequest.asStateFlow()
+    private val _createMissionUiState = MutableStateFlow<CreateMissionUiState>(CreateMissionUiState.Loading)
+    val createMissionUiState = _createMissionUiState.asStateFlow()
+
+    private val _title = mutableStateOf("")
+    val title = _title
+
+    private val _content = mutableStateOf("")
+    val content = _content
+
+    private val _minute = mutableIntStateOf(0)
+    val minute = _minute
+
+    private val _second = mutableIntStateOf(0)
+    val second = _second
+
+    private val _timeLimit = mutableIntStateOf(0)
+    val timeLimit = _timeLimit
 
     internal fun getMission() = viewModelScope.launch {
         getMissionUseCase()
-            .onSuccess {
-                it.catch { remoteError ->
-                    _missionRequest.value = remoteError.errorHandling()
-                }.collect { response ->
-                    _missionRequest.value = Event.Success(data = response)
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _missionUiState.value = GetMissionUiState.Loading
+                    is Result.Success -> _missionUiState.value = GetMissionUiState.Success(result.data)
+                    is Result.Error -> _missionUiState.value = GetMissionUiState.Error(result.exception)
                 }
-            }
-            .onFailure {
-                _missionRequest.value = it.errorHandling()
             }
     }
 
@@ -66,10 +87,32 @@ class MissionViewModel @Inject constructor(
     internal fun createMission(body: CreateMissionRequestModel) = viewModelScope.launch {
         createMissionUseCase(body = body)
             .onSuccess {
-                _createMissionRequest.value = Event.Success()
+                _createMissionUiState.value = CreateMissionUiState.Success
+                Log.e("Mission_Success", _createMissionUiState.value.toString())
             }
             .onFailure {
-                _createMissionRequest.value = it.errorHandling()
+                _createMissionUiState.value = CreateMissionUiState.Error(it)
+                Log.e("Mission_Failure", _createMissionUiState.value.toString())
             }
+    }
+
+    internal fun onTitle(value: String) {
+        _title.value = value
+    }
+
+    internal fun onContent(value: String) {
+        _content.value = value
+    }
+
+    internal fun onMinute(value: Int) {
+        _minute.intValue = value
+    }
+
+    internal fun onSecond(value: Int) {
+        _second.intValue = value
+    }
+
+    internal fun onTimeLimit() {
+        _timeLimit.intValue = (minute.intValue * 60) + second.intValue
     }
 }
