a9f5217b6fe1e0381846d7275ca2925dcd9983a1
GSM-Minseok <ms0818000@gmail.com>
2024-07-03 17:54:04 +0900

    :sparkles: :: Use GetMissionUseCase

--- feature/main/src/main/java/com/stackknowledge/main/viewModel/MainViewModel.kt
+++ feature/main/src/main/java/com/stackknowledge/main/viewModel/MainViewModel.kt
@@ -1,14 +1,36 @@
 package com.stackknowledge.main.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.common.result.Result
+import com.example.common.result.asResult
+import com.example.common.util.Event
+import com.stackknowledge.main.viewModel.uistate.GetMissionUiState
 import com.stackknowledge.usecase.mission.GetMissionUseCase
 import com.stackknowledge.usecase.solve.SolveUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     val getMissionUseCase: GetMissionUseCase
 ): ViewModel() {
+    private val _getMissionUiState = MutableStateFlow<GetMissionUiState>(GetMissionUiState.Loading)
+    val getMissionUiState = _getMissionUiState.asStateFlow()
 
+    fun getMission() = viewModelScope.launch {
+        getMissionUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when(result) {
+                    is Result.Loading -> _getMissionUiState.value = GetMissionUiState.Loading
+                    is Result.Success -> _getMissionUiState.value = GetMissionUiState.Success(result.data)
+                    is Result.Error -> _getMissionUiState.value = GetMissionUiState.Error(result.exception)
+                }
+            }
+    }
 }
