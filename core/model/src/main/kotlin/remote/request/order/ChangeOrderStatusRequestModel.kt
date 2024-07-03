4cf87996ce769b8fbaa39979e86023875595db13
GSM-Minseok <ms0818000@gmail.com>
2024-07-03 12:01:10 +0900

    :memo: :: modify order

--- core/model/src/main/kotlin/remote/request/order/ChangeOrderStatusRequestModel.kt
+++ core/model/src/main/kotlin/remote/request/order/ChangeOrderStatusRequestModel.kt
@@ -1,11 +1,8 @@
 package remote.request.order
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 import java.util.UUID
 
-@JsonClass(generateAdapter = true)
-data class ChangeOrderStatusRequestModel (
-    @Json(name = "orderId") val orderId: UUID,
-    @Json(name = "count") val count: Int,
+data class ChangeOrderStatusRequestModel(
+    val orderId: UUID,
+    val count: Int,
 )
