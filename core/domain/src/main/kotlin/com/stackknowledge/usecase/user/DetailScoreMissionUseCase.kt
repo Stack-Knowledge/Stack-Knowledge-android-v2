054f9303d949417ebd0c6bb83eecc596e7eed0ba
yeongun130 <yeongunj625@gmail.com>
2024-07-04 22:48:24 +0900

    :sparkles: :: Add Score UseCase

---
+++ core/domain/src/main/kotlin/com/stackknowledge/usecase/user/DetailScoreMissionUseCase.kt
@@ -0,0 +1,12 @@
+package com.stackknowledge.usecase.user
+
+import com.stackknowledge.repository.user.UserRepository
+import java.util.UUID
+import javax.inject.Inject
+
+class DetailScoreMissionUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke(solveId: UUID) =
+        userRepository.getDetailSolveMission(solveId = solveId)
+}
