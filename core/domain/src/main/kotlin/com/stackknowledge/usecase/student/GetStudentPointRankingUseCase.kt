d522a7d116c02490d9faf3825d41f1abe0a2135a
GSM-Minseok <ms0818000@gmail.com>
2024-07-04 23:30:48 +0900

    :memo: :: Add GetStudentPointRankingUseCase

--- core/domain/src/main/kotlin/com/stackknowledge/usecase/student/GetStudentPointRankingUseCase.kt
+++ core/domain/src/main/kotlin/com/stackknowledge/usecase/student/GetStudentPointRankingUseCase.kt
@@ -1,12 +1,13 @@
 package com.stackknowledge.usecase.student
 
 import com.stackknowledge.repository.student.StudentRepository
+import kotlinx.coroutines.flow.Flow
+import remote.response.student.GetStudentPointRankingResponseModel
 import javax.inject.Inject
 
 class GetStudentPointRankingUseCase @Inject constructor(
     private val studentRepository: StudentRepository
 ) {
-    operator fun invoke() {
+    operator fun invoke(): Flow<List<GetStudentPointRankingResponseModel>> =
         studentRepository.getStudentPointRaking()
-    }
 }
