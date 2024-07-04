6537272bc9dff799827ce085a13b784fd745b7e1
GSM-Minseok <ms0818000@gmail.com>
2024-07-04 17:50:49 +0900

    :sparkles: :: Add ResourceKeys.kt

--- core/network/src/main/kotlin/com/stackknowledge/util/AuthInterceptor.kt
+++ core/network/src/main/kotlin/com/stackknowledge/util/AuthInterceptor.kt
@@ -3,6 +3,7 @@ package com.stackknowledge.util
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import util.ResourceKeys
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(): Interceptor {
@@ -11,7 +12,7 @@ class AuthInterceptor @Inject constructor(): Interceptor {
         val builder = request.newBuilder()
 
         runBlocking {
-            builder.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZjQzNzZjOS03MmZhLTQxYjQtYTg0NS04ZmYxNTQ2MzU4MTEiLCJ0eXBlIjoiYWNjZXNzIiwiYXV0aG9yaXR5IjoiUk9MRV9URUFDSEVSIiwiaWF0IjoxNzIwMDgyMjk0LCJleHAiOjE3MjAwODQwOTR9.Jvyx3XH3URBBF-5bfb-YxP_agLq3bFdXqsnSOk0l-W8")
+            builder.addHeader("Authorization", "${ResourceKeys.BEARER} ")
         }
         return chain.proceed(builder.build())
     }
