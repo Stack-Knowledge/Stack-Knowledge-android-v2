5f224693464aea44c532f685e62746406fd33fad
GSM-Minseok <ms0818000@gmail.com>
2024-07-08 15:45:49 +0900

    :sparkles: :: Modify ranking profile image

--- feature/ranking/src/main/java/com/stackknowledge/ranking/component/RankingItem.kt
+++ feature/ranking/src/main/java/com/stackknowledge/ranking/component/RankingItem.kt
@@ -8,16 +8,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.stackknowledge.design_system.theme.StackKnowledgeAndroidTheme
 import com.stackknowledge.design_system.R
 
@@ -48,13 +52,23 @@ fun RankingItem(
                 color = colors.BLACK
             )
             Spacer(modifier = modifier.width(8.dp))
-            Image(
-                painter = painterResource(R.drawable.img_profile),
-                contentDescription = "Profile",
-                modifier = modifier
-                    .width(40.dp)
-                    .height(40.dp)
-            )
+            if (profileImage.isNullOrEmpty()) {
+                Image(
+                    painter = painterResource(R.drawable.img_profile),
+                    contentDescription = "Profile",
+                    modifier = modifier
+                        .width(40.dp)
+                        .height(40.dp)
+                )
+            } else {
+                AsyncImage(
+                    model = profileImage,
+                    contentDescription = "Profile",
+                    modifier = modifier
+                        .width(40.dp)
+                        .height(40.dp)
+                )
+            }
             Spacer(modifier = modifier.width(8.dp))
             Text(
                 text = name,
