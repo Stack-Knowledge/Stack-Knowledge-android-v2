55ba6f2d5cefcd021d47e512d654670d317765f9
GSM-Minseok <ms0818000@gmail.com>
2024-07-08 17:40:21 +0900

    :sparkles: :: Use getMyInformationUseCase

--- feature/ranking/src/main/java/com/stackknowledge/ranking/viewModel/RankingViewModel.kt
+++ feature/ranking/src/main/java/com/stackknowledge/ranking/viewModel/RankingViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.common.result.Result
 import com.example.common.result.asResult
+import com.stackknowledge.ranking.viewModel.uistate.GetMyInformationUiState
 import com.stackknowledge.ranking.viewModel.uistate.GetRankingUiState
 import com.stackknowledge.usecase.mission.GetMissionUseCase
+import com.stackknowledge.usecase.student.GetMyInformationUseCase
 import com.stackknowledge.usecase.student.GetStudentPointRankingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,25 +20,41 @@ import javax.inject.Inject
 @HiltViewModel
 class RankingViewModel @Inject constructor(
     private val getStudentPointRankingUseCase: GetStudentPointRankingUseCase,
+    private val getMyInformationUseCase: GetMyInformationUseCase,
 ): ViewModel() {
     private val _getRankingUiState = MutableStateFlow<GetRankingUiState>(GetRankingUiState.Loading)
     internal val getRankingUiState = _getRankingUiState.asStateFlow()
+
+    private val _getMyInformationUiState = MutableStateFlow<GetMyInformationUiState>(GetMyInformationUiState.Loading)
+    internal val getMyInformationUiState = _getMyInformationUiState.asStateFlow()
     internal fun getRanking() = viewModelScope.launch {
         getStudentPointRankingUseCase()
             .asResult()
             .collectLatest { result ->
                 when(result) {
+                    is Result.Loading -> _getRankingUiState.value = GetRankingUiState.Loading
+                    is Result.Success -> _getRankingUiState.value = GetRankingUiState.Success(result.data)
+                    is Result.Error -> _getRankingUiState.value = GetRankingUiState.Error(result.exception)
+                }
+            }
+    }
+
+    internal fun getMyProfile() = viewModelScope.launch {
+        getMyInformationUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when(result) {
                     is Result.Loading -> {
                         Log.d("testt","load")
-                        _getRankingUiState.value = GetRankingUiState.Loading
+                        _getMyInformationUiState.value = GetMyInformationUiState.Loading
                     }
                     is Result.Success -> {
                         Log.d("testt","suc")
-                        _getRankingUiState.value = GetRankingUiState.Success(result.data)
+                        _getMyInformationUiState.value = GetMyInformationUiState.Success(result.data)
                     }
                     is Result.Error -> {
                         Log.d("testt",result.exception.toString())
-                        _getRankingUiState.value = GetRankingUiState.Error(result.exception)
+                        _getMyInformationUiState.value = GetMyInformationUiState.Error(result.exception)
                     }
                 }
             }
