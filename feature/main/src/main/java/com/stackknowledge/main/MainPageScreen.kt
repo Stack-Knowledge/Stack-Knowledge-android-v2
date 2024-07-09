e3f3eba8e8f02b89758905f3ee1ba7742412d11c
yeongun130 <yeongunj625@gmail.com>
2024-07-09 11:17:04 +0900

    :memo: :: modify Dialog

--- feature/main/src/main/java/com/stackknowledge/main/MainPageScreen.kt
+++ feature/main/src/main/java/com/stackknowledge/main/MainPageScreen.kt
@@ -119,7 +119,7 @@ private fun MainPageScreen(
         if (openLogoutDialog) {
             StackKnowledgeDialog(
                 content = "로그아웃 하시겠습니까?",
-                numberOfButton = 2,
+                isTimeOut = false,
                 onConfirm = { openLogoutDialog = false },
                 onDismiss = { openLogoutDialog = false },
                 onStateChange = { openLogoutDialog = it },
