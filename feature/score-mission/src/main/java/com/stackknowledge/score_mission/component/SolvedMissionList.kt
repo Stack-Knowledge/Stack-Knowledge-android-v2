2dacd4300fa38da6e3fabbd0a5fb4f5bbcae5827
yeongun130 <yeongunj625@gmail.com>
2024-07-09 14:41:17 +0900

    :memo: :: modify ScoreMissionList ratio

--- feature/score-mission/src/main/java/com/stackknowledge/score_mission/component/SolvedMissionList.kt
+++ feature/score-mission/src/main/java/com/stackknowledge/score_mission/component/SolvedMissionList.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,7 +48,9 @@ fun SolvedMissionList(
                 ),
             ) {
                 itemsIndexed(scoreMission.response) {_, item ->
-                    Box {
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
                         SolvedMissionItem(
                             name = item.user.name,
                             title = item.title,
