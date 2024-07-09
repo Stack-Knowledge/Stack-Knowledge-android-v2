797a1631a343e6bb459a1b0f3a2296dc51afc657
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-09 14:42:27 +0900

    ✨:: ItemViewModel GetMyInformationUiState 적용

--- feature/shop/src/main/java/com/stackknowledge/shop/viewmodel/ItemViewModel.kt
+++ feature/shop/src/main/java/com/stackknowledge/shop/viewmodel/ItemViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.example.common.result.Result
 import com.example.common.result.asResult
 import com.stackknowledge.shop.viewmodel.uistate.GetItemUiState
+import com.stackknowledge.shop.viewmodel.uistate.GetMyInformationUiState
 import com.stackknowledge.usecase.item.GetItemUseCase
+import com.stackknowledge.usecase.student.GetMyInformationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,9 +18,14 @@ import javax.inject.Inject
 @HiltViewModel
 class ItemViewModel @Inject constructor(
     private val getItemUseCase: GetItemUseCase,
+    private val getMyInformationUseCase: GetMyInformationUseCase,
 ) : ViewModel() {
     private val _getItemUiState = MutableStateFlow<GetItemUiState>(GetItemUiState.Loading)
     internal val getItemUiState = _getItemUiState.asStateFlow()
+
+    private val _getMyInformationUiState =
+        MutableStateFlow<GetMyInformationUiState>(GetMyInformationUiState.Loading)
+    internal val getMyInformationUiState = _getMyInformationUiState.asStateFlow()
 
     internal fun getItem() = viewModelScope.launch {
         getItemUseCase()
@@ -27,7 +34,20 @@ class ItemViewModel @Inject constructor(
                 when (result) {
                     is Result.Loading -> _getItemUiState.value = GetItemUiState.Loading
                     is Result.Success -> _getItemUiState.value = GetItemUiState.Success(result.data)
-                    is Result.Error -> _getItemUiState.value = GetItemUiState.Error(result.exception)
+                    is Result.Error -> _getItemUiState.value =
+                        GetItemUiState.Error(result.exception)
+                }
+            }
+    }
+
+    internal fun getMyInformation() = viewModelScope.launch {
+        getMyInformationUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _getMyInformationUiState.value = GetMyInformationUiState.Loading
+                    is Result.Success -> _getMyInformationUiState.value = GetMyInformationUiState.Success(result.data)
+                    is Result.Error -> _getMyInformationUiState.value = GetMyInformationUiState.Error(result.exception)
                 }
             }
     }
