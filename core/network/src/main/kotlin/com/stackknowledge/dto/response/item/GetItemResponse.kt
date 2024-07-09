f2f42644a1484eeb024708223b6990cf71f41788
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-09 17:44:45 +0900

    🔥️:: GetItemResponse 불필요한 import문 삭제

--- core/network/src/main/kotlin/com/stackknowledge/dto/response/item/GetItemResponse.kt
+++ core/network/src/main/kotlin/com/stackknowledge/dto/response/item/GetItemResponse.kt
@@ -2,8 +2,6 @@ package com.stackknowledge.dto.response.item
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import com.stackknowledge.dto.item.Item
-import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class GetItemResponse(
