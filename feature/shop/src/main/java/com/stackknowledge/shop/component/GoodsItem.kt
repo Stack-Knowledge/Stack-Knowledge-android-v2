2a10882a241e42b4ce3f93402355ae500a0a8135
yeongun130 <yeongunj625@gmail.com>
2024-07-11 11:19:16 +0900

    :memo: :: Item clip 추가

--- feature/shop/src/main/java/com/stackknowledge/shop/component/GoodsItem.kt
+++ feature/shop/src/main/java/com/stackknowledge/shop/component/GoodsItem.kt
@@ -11,12 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -49,6 +51,7 @@ fun GoodsItem(
         ) {
             Box(
                 modifier = modifier
+                    .clip(shape = RoundedCornerShape(10.dp))
                     .clickable {
                         if (checked.value) {
                             onItemCheckButtonClick(itemData)
