432e39070adf56afa1633d36022e9b3f33452f15
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-11 16:19:18 +0900

    ✨:: core/data단 build.gradle.kts moshi 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -16,4 +16,5 @@ dependencies {
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.moshi.converter)
+    implementation(libs.retrofit.moshi.codegen)
 }
