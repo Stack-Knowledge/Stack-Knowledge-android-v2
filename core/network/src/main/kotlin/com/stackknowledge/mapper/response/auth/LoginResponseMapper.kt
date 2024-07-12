3d179ab12620f68e138de67e0b0a602e6a073fc1
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-12 09:56:48 +0900

    ♻️:: LoginResponseMapper this 접근 추가

--- core/network/src/main/kotlin/com/stackknowledge/mapper/response/auth/LoginResponseMapper.kt
+++ core/network/src/main/kotlin/com/stackknowledge/mapper/response/auth/LoginResponseMapper.kt
@@ -4,8 +4,8 @@ import com.stackknowledge.dto.response.auth.LoginResponse
 import remote.response.auth.LoginResponseModel
 
 fun LoginResponse.toModel(): LoginResponseModel = LoginResponseModel(
-    accessToken = accessToken,
-    refreshToken = refreshToken,
-    expiredAt = expiredAt,
-    authority = authority,
+    accessToken = this.accessToken,
+    refreshToken = this.refreshToken,
+    expiredAt = this.expiredAt,
+    authority = this.authority,
 )
