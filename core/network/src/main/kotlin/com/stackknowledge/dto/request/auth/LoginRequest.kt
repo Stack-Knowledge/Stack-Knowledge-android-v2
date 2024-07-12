0fa43ff19298d5fb9315ee11ba1b2025bad2573d
Chaejongin12 <s22055@gsm.hs.kr>
2024-07-12 09:57:06 +0900

    ♻️:: LoginRequest 마지막 필드 , 추가

--- core/network/src/main/kotlin/com/stackknowledge/dto/request/auth/LoginRequest.kt
+++ core/network/src/main/kotlin/com/stackknowledge/dto/request/auth/LoginRequest.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class LoginRequest(
-    @Json(name = "code") val code: String
+    @Json(name = "code") val code: String,
 )
