1a016322909449437fa6bc4053a3e2fb467b8774
GSM-Minseok <ms0818000@gmail.com>
2024-07-15 12:10:18 +0900

    :lipstick: :: Add Divider

--- core/ui/src/main/java/com/minstone/ui/navigation/StackKnowledgeBottomNavigation.kt
+++ core/ui/src/main/java/com/minstone/ui/navigation/StackKnowledgeBottomNavigation.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Divider
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -41,35 +43,41 @@ fun StackKnowledgeBottomNavigation(
             modifier = modifier.background(color = colors.WHITE),
             contentAlignment = Alignment.BottomCenter
         ) {
-            Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                val navItems = listOf(
-                    Triple(R.drawable.home_icon, NavigateType.HOME.value, R.string.home),
-                    Triple(R.drawable.mission_icon, NavigateType.MISSION.value, if(role == Authority.ROLE_STUDENT) R.string.mission else R.string.solved_mission),
-                    Triple(R.drawable.create_mission_icon, NavigateType.CREATE_MISSION.value, R.string.create_mission),
-                    Triple(R.drawable.shop_icon, NavigateType.SHOP.value, R.string.shop),
-                    Triple(R.drawable.ranking_icon, NavigateType.RANKING.value, R.string.ranking)
+            Column {
+                Divider(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color(0xFFF1F1F1)
                 )
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val navItems = listOf(
+                        Triple(R.drawable.home_icon, NavigateType.HOME.value, R.string.home),
+                        Triple(R.drawable.mission_icon, NavigateType.MISSION.value, if(role == Authority.ROLE_STUDENT) R.string.mission else R.string.solved_mission),
+                        Triple(R.drawable.create_mission_icon, NavigateType.CREATE_MISSION.value, R.string.create_mission),
+                        Triple(R.drawable.shop_icon, NavigateType.SHOP.value, R.string.shop),
+                        Triple(R.drawable.ranking_icon, NavigateType.RANKING.value, R.string.ranking)
+                    )
 
-                navItems.forEachIndexed { index, (iconRes, navigateType, stringResId) ->
-                    if (index != 2 || role == Authority.ROLE_TEACHER) {
-                        BottomNavigationComponent(
-                            modifier = modifier
-                                .weight(1f)
-                                .height(56.dp),
-                            buttonText = stringResId,
-                            buttonImage = {
-                                Image(
-                                    painter = painterResource(iconRes),
-                                    contentDescription = null
-                                )
+                    navItems.forEachIndexed { index, (iconRes, navigateType, stringResId) ->
+                        if (index != 2 || role == Authority.ROLE_TEACHER) {
+                            BottomNavigationComponent(
+                                modifier = modifier
+                                    .weight(1f)
+                                    .height(56.dp),
+                                buttonText = stringResId,
+                                buttonImage = {
+                                    Image(
+                                        painter = painterResource(iconRes),
+                                        contentDescription = null
+                                    )
+                                }
+                            ) {
+                                onNavigate(navigateType)
                             }
-                        ) {
-                            onNavigate(navigateType)
                         }
                     }
                 }
